6fcee767ad8cb6410e7df0511195fc13f2a4edee
julian_17 <julian.stamm@gmx.net>
2025-01-31 23:46:07 +0100

    Working on fixing erros ----- still usin gold TF instead of Keras

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from tensorflow import feature_column as fc
 import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+
 
 dftrain = pd.read_csv('car_train.csv')
 dfeval = pd.read_csv('car_eval.csv')
 
+print("Train Columns:", dftrain.columns)
+print("Eval Columns:", dfeval.columns)
+
 y_train = dftrain.pop('Price')
 y_eval = dfeval.pop('Price')
 
+
+
 categorical_columns = ['Brand', 'Model', 'Fuel_Type', 'Transmission']
+
 numeric_columns = ['Year', 'Engine_Size', 'Mileage', 'Doors', 'Owner_Count']
 
 feature_columns = []
@@ -19,32 +29,24 @@ for feature_name in categorical_columns:
     feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
 
 for feature_name in numeric_columns:
-    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
-
-model = tf.keras.Sequential([
-    tf.keras.layers.InputLayer(input_shape=(len(categorical_columns) + len(numeric_columns),)),
-    feature_layer,
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(1, activation='linear')  # Use 'sigmoid' for binary classification
-])
-
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-
-def make_dataset(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
     if shuffle:
-        ds = ds.shuffle(buffer_size=len(data_df))
-    ds = ds.batch(batch_size).repeat(num_epochs)
-    return ds
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
 
-train_ds = make_dataset(dftrain, y_train, num_epochs=10, shuffle=True, batch_size=32)
-eval_ds = make_dataset(dfeval, y_eval, num_epochs=1, shuffle=False, batch_size=32)
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
-steps_per_epoch = len(dftrain) // 32
-model.fit(train_ds, epochs=10, steps_per_epoch=steps_per_epoch)
 
-eval_steps = len(dfeval) // 32
-result = model.evaluate(eval_ds, steps=eval_steps)
-print("Accuracy:", result[1])
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
