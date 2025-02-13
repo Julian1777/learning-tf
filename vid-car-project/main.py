82dc175e0a4e22a27a75c54b83c66913466f1b79
Julian <104464142+Julian1777@users.noreply.github.com>
2025-02-13 11:32:11 +0100

    Created Keras Sequential Model. Working on optimizing

--- vid-car-project/main.py
+++ vid-car-project/main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
-from keras.layers import Normalization
-from tensorflow.keras.layers import Normalization
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Normalization, Dense, InputLayer
+from tensorflow.keras.losses import MeanSquaredError, Huber, MeanAbsoluteError
+from tensorflow.keras.optimizer import Adam
 
 
 data = pd.read_csv('train.csv')
@@ -23,7 +24,29 @@ y = tensor_data[:,-1]
 y = tf.expand_dims(y, axis = -1)
 print(y[:5])
 
-normalizer = Normalization(mean = 5, variance = 4)
-x_normalized = tf.constant([[3,4,5,6,7]])
-normalizer(x_normalized)
-print(x_normalized)
+normalizer = Normalization()
+x_normalized = tf.constant([[3,4,5,6,7],
+                            [4,5,6,7,8],
+                            [32,1,56,3,5]])
+print(normalizer.adapt(x_normalized))
+print(normalizer(x_normalized))
+
+print(x.shape)
+
+normalizer = Normalization(input_shape=(x.shape[1],))
+normalizer.adapt(x)
+print(normalizer(x)[:5])
+
+
+model = keras.Sequential([
+    InputLayer(input_shape = (8,)),
+    normalizer,
+    layers.Dense(1)
+])
+
+print(model.summary())
+
+
+tf.keras.utils.plot_model(model, to_file='model.png', show_shapes = True)
+model.compile(optimizer = Adam(), loss = MeanAbsoluteError())
+history = model.fit(x,y, epochs = 100, verbose = 1)
