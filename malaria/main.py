669fa5fcc31978bcf5d9053649a0416dad1b52fe
julian_17 <julian.stamm@gmx.net>
2025-02-18 18:15:49 +0100

    Started working on the malaria project

---
+++ malaria/main.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import numpy as np
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+#LOADING DATASET AND SPLITTING INTO TRAIN VAL AND TEST
+
+dataset, dataset_info = tfds.load('malaria', with_info=True, as_supervised=True, shuffle_files=True, split=['train'])
+
+def splits(dataset, TRAIN_RATIO, VAL_RATIO, TEST_RATIO):
+    DATASET_SIZE = len(dataset)
+
+    train_dataset = dataset.take(int(TRAIN_RATIO*DATASET_SIZE))
+    val_test_dataset = dataset.skip(int(TRAIN_RATIO*DATASET_SIZE))
+    val_dataset = val_test_dataset.take(int(VAL_RATIO*DATASET_SIZE))
+    test_dataset = val_test_dataset.skip(int(VAL_RATIO*DATASET_SIZE))
+
+    return train_dataset, val_dataset, test_dataset
+
+
+
+TRAIN_RATIO = 0.8
+VAL_RATIO = 0.1
+TEST_RATIO = 0.1
+
+#dataset = tf.data.Dataset.range(10)
+train_dataset, val_dataset, test_dataset = splits(dataset[0], TRAIN_RATIO, VAL_RATIO, TEST_RATIO)
+print(list(train_dataset.take(1).as_numpy_iterator()), list(val_dataset.take(1).as_numpy_iterator()), list(test_dataset.take(1).as_numpy_iterator()))
+
+
+
+#DATASET VISUALIZATION
+
+for i, (image,label) in enumerate(train_dataset.take(16)):
+    ax = plt.subplot(4, 4, i+1)
+    plt.imshow(image)
+    plt.title(dataset_info.features['label'].int2str(label))
+    plt.axis('off')
+
+plt.show()
+
+
+#DATA PROCESSING
+
